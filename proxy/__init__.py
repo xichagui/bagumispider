d2bca5d0dc5fecf471f97f26e352fc92e5df861d
xichagui <xichagui@gmail.com>
2020-07-18 17:09:57 +0800

    init.

---
+++ proxy/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/18 16:57
+# @Author  : Xichagui
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ proxy/proxy_spider.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/18 16:58
+# @Author  : Xichagui
+# @Site    : 
+# @File    : proxy_spider.py
+# @Software: PyCharm
